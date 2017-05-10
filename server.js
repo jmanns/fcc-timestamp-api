1b011407961917af8cb7b7f8972b0cd253f667a4
Jason Manns <Jason.Manns@gmx.com>
2017-05-10 19:46:33 +0100

    Completed the 3 user stories

--- server.js
+++ server.js
@@ -2,9 +2,38 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({
-    test: 'test'
-  })
+  res.send('<div style="height:100vh;display:flex;align-items:center;justify-content:center;"><h1 style="color:#999;">Freecodecamp Timestamp API</h1></div>')
+})
+
+app.get('/:input', (req, res) => {
+  const { input } = req.params
+  
+  if (isTimestamp(input)) {
+    res.json({
+      unix: +input,
+      natural: convertTimestamp(+input)
+    })
+  } else if (Date.parse(input)) {
+    res.json({
+      unix: Date.parse(input),
+      natural: input
+    })
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    })
+  }
+
 })
 
 app.listen(3000)
+
+function isTimestamp (str) {
+  return !isNaN(str)
+}
+
+function convertTimestamp (value) {
+  const date = new Date(value)
+  return date.toDateString()
+}
